78c2079f1af12a42e6c1315e4431718a2adbd154
Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
2016-11-12 15:48:17 -0700

    lux: Set TARGET_BOOTANIMATION_HALF_RES to true
    * No apparent resolution difference
    * Much smoother animation
    * Much faster boot
    * What's not to like?
    
    Change-Id: I2119fe826146a3103da50b4862ad88f5950a97ec

--- lineage_lux.mk
+++ lineage_lux.mk
@@ -24,6 +24,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Boot animation
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 1920
+TARGET_BOOTANIMATION_HALF_RES := true
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := lux
