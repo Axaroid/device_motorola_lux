909b06d9234d57f4369c7cfe74b56e8577080095
althafvly <althafvly@gmail.com>
2020-04-24 09:18:40 +0000

    pixelify

--- aosp_lux.mk
+++ aosp_lux.mk
@@ -19,16 +19,22 @@ $(call inherit-product, device/motorola/lux/full_lux.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 1920
+TARGET_BOOT_ANIMATION_RES := 720
 TARGET_BOOTANIMATION_HALF_RES := true
-
+TARGET_GAPPS_ARCH := arm
+IS_PHONE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_MINIMAL_APPS := true
+TARGET_INCLUDE_WIFI_EXT := false
+$(shell rm -rf vendor/google/customization/interfaces/wifi_ext)
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := lux
-PRODUCT_NAME := lineage_lux
+PRODUCT_NAME := aosp_lux
 PRODUCT_MODEL := Moto X Play
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
